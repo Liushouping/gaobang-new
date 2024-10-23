dadc2c1bfadd87202bd26410346570cf4376bace
liushouping <kose146352@gmail.com>
2024-10-23 20:32:39 +0800

    Test

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -2551,7 +2551,7 @@ class="relative overflow-hidden">
     </p>
   </section>
   <!--  -->
-  <Footer 
+  <footer 
   class="fixed bottom-5 w-full flex flex-col justify-center items-center">
     <div 
     class="flex flex-col justify-center items-center">
@@ -2559,6 +2559,6 @@ class="relative overflow-hidden">
       class="text-white text-[10px] font-GenJyuuGothicRegular" 
       href="mailto:pkbaby0707@gmail.com">{{ Bottomdata.updatedInfo }}</a>
     </div>
-  </Footer>
+  </footer>
 </div>
 </template>
